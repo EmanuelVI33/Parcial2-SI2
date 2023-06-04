7dadb45ebdee0fde9efdc5b7f532f4d17fe07022
Emanuel <emanuelvaca39@gmail.com>
2023-06-04 03:40:55 -0400

    First commit

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const String google_api_key = "AIzaSyBXK0_IOeU2iJ3LN7pFQsDMR-CVrV7fkOU";
+const Color primaryColor = Color(0xFF7B61FF);
+const double defaultPadding = 16.0;
